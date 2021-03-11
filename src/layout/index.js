f8f71fee1f5734187ec43838d013d51a5002b0dd
Min <solunar@vip.qq.com>
2021-03-11 22:51:59 +0800

    update

---
+++ src/layout/index.js
@@ -0,0 +1,14 @@
+import './index.scss';
+
+const message = 'Lorem ipsum dolor sit amet.';
+
+function Layout() {
+  return (
+    <div className="layout">
+      <h1>layout</h1>
+      <p>{message}</p>
+    </div>
+  );
+}
+
+export default Layout;
