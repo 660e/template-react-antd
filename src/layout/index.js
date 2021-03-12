6d0fc48d87bf103ee0e3713990e7a0f828930116
Min <solunar@vip.qq.com>
2021-03-12 17:23:43 +0800

    update

--- src/layout/index.js
+++ src/layout/index.js
@@ -1,5 +1,5 @@
 import styles from './index.module.scss';
-import { Layout } from 'antd';
+import { Layout, Menu } from 'antd';
 
 const { Sider, Content } = Layout;
 
@@ -7,7 +7,19 @@ function MainLayout() {
   return (
     <Layout className={styles['ant-layout']}>
       <Layout>
-        <Sider>Sider</Sider>
+        <Sider>
+          <Menu className={styles['ant-menu-vertical']} theme="dark">
+            <Menu.Item className={styles['ant-menu-item']} key="1">
+              AAA
+            </Menu.Item>
+            <Menu.Item className={styles['ant-menu-item']} key="2">
+              BBB
+            </Menu.Item>
+            <Menu.Item className={styles['ant-menu-item']} key="3">
+              CCC
+            </Menu.Item>
+          </Menu>
+        </Sider>
         <Content>Content</Content>
       </Layout>
     </Layout>
