64d6fb0ef3af4b1163f4ff58238ff7fb003a2a9b
Min <solunar@vip.qq.com>
2021-03-17 23:16:02 +0800

    init form

--- src/views/demo/form.jsx
+++ src/views/demo/form.jsx
@@ -2,41 +2,42 @@ import { Component } from 'react';
 import { Button, Form, Input } from 'antd';
 
 const rules = {
-  phone: [{ required: true, message: 'required' }],
+  phone: [
+    { required: true, message: 'required' },
+    { pattern: /^1[3|4|5|7|8][0-9]{9}$/, message: 'not valid' }
+  ],
   password: [{ required: true, message: 'required' }],
-  verifyPassword: [{ required: true, message: 'required' }]
+  confirmPassword: [
+    { required: true, message: 'required' },
+    ({ getFieldValue }) => ({
+      validator(rule, value) {
+        if (!value || getFieldValue('password') === value) {
+          return Promise.resolve();
+        }
+        return Promise.reject(new Error('not match'));
+      }
+    })
+  ]
 };
 
 class FormComponent extends Component {
-  constructor() {
-    super();
-    this.state = {
-      forms: {
-        phone: '',
-        password: '',
-        verifyPassword: '',
-        tag: ''
-      }
-    };
-  }
-
-  onFinish = () => {
-    console.log('onFinish');
+  onFinish = values => {
+    console.log(values);
   };
 
   render() {
     return (
-      <Form style={{ width: 420 }} labelCol={{ span: 8 }} wrapperCol={{ span: 16 }} onFinish={this.onFinish}>
-        <Form.Item label="Phone" name="phone" rules={rules.phone}>
+      <Form style={{ width: 500 }} labelCol={{ span: 8 }} wrapperCol={{ span: 16 }} onFinish={this.onFinish}>
+        <Form.Item label="Phone" name="phone" rules={rules.phone} hasFeedback>
           <Input />
         </Form.Item>
-        <Form.Item label="Password" name="password" rules={rules.password}>
+        <Form.Item label="Password" name="password" rules={rules.password} hasFeedback>
           <Input />
         </Form.Item>
-        <Form.Item label="VerifyPassword" name="verifyPassword" rules={rules.verifyPassword}>
+        <Form.Item label="Confirm Password" name="confirmPassword" rules={rules.confirmPassword} dependencies={['password']} hasFeedback>
           <Input />
         </Form.Item>
-        <Form.Item label="Tag">
+        <Form.Item label="Tag" name="tag">
           <Input />
         </Form.Item>
         <Form.Item wrapperCol={{ offset: 8, span: 16 }}>
